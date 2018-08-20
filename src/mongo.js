bbac08c7f9fac643ef145b3badd3429cfd644866
cjwddz <cjwddz@qq.com>
2018-08-20 21:29:31 +0800

    [add]

--- src/mongo.js
+++ src/mongo.js
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
-import config from 'config'
+import config from './config'
 
 let _db, _schema, statisticsModel
 
---
+++ src/server.js
@@ -0,0 +1,22 @@
+'use strict'
+var mongo = require('./mongo').default
+
+mongo.initDB(function (err) {
+  if (err) {
+    console.log('init db error:', err)
+    throw err
+  }
+  if (!module.parent) {
+
+    var http = require('http');
+
+    http.createServer(function (request, response) {
+
+      response.writeHead(200, {'Content-Type': 'text/plain'})
+      // 发送响应数据 "Hello World"
+      response.end('Hello World\n');
+    }).listen(config.get('port'))
+    // 终端打印如下信息
+    console.log('Server running at http://127.0.0.1:8888/')
+  }
+})
