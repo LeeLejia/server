0bc0d2ff52fd73179d97375221d6342734c520be
cjwddz <cjwddz@qq.com>
2018-08-26 00:15:05 +0800

    [mdf]

--- config/development.js
+++ config/development.js
@@ -3,7 +3,7 @@
 module.exports = {
   ...require('./default'),
   mongodb: {
-    uri: process.env.MONGODB || 'mongodb://admin:admin123@127.0.0.1:27017/performance',
+    uri: process.env.MONGODB || 'mongodb://calculate:calculate@120.131.11.64:991/performance',
     debug: true,
   },
   redis: {
