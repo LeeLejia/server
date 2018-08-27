d47f082c3d3942bd899487fb637ed80dde7d73e2
cjwddz <cjwddz@qq.com>
2018-08-27 17:54:51 +0800

    [mdf]

--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@
 module.exports = {
   ...require('./default'),
   mongodb: {
-    uri: process.env.MONGODB || 'mongodb://admin:admin123@127.0.0.1:27017/performance',
+    uri: process.env.MONGODB || 'mongodb://cjwddz:imjia123@127.0.0.1/knowuLog',
     debug: false,
   },
   redis:{
