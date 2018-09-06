827e0c8a241d453f55dabd759aeaace08c3f722e
cjwddz <cjwddz@qq.com>
2018-09-06 17:52:50 +0800

    [mdf] 添加knowu接口

--- config/development.js
+++ config/development.js
@@ -3,7 +3,7 @@
 module.exports = {
   ...require('./default'),
   mongodb: {
-    uri: process.env.MONGODB || "mongodb://www.cjwddz.cn/knowu",
+    uri: process.env.MONGODB || "mongodb://mongodb/knowu",
     debug: false,
   },
   redis:{
